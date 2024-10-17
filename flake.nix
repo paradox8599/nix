dbe595fa9af03de454a8a9636b8f57b678cade78
paradox8599 <paradox8599@outlook.com>
2024-10-17 05:03:43 +0000

    init working configs

---
+++ flake.nix
@@ -0,0 +1,87 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+    nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , nixos-wsl
+    , ...
+    }: {
+      nixosConfigurations = {
+        nixos = nixpkgs.lib.nixosSystem rec {
+          system = "x86_64-linux";
+          modules =
+            let
+              pkgs = nixpkgs.legacyPackages.${system};
+              username = "nixos";
+            in
+            [
+              nixos-wsl.nixosModules.default
+              # configuration.nix
+              {
+                system.stateVersion = "24.05";
+                nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+                wsl.enable = true;
+                wsl.defaultUser = "${username}";
+                wsl.docker-desktop.enable = true;
+                wsl.startMenuLaunchers = true;
+                wsl.wslConf.user.default = "${username}";
+
+                environment.systemPackages = with pkgs; [
+                  neovim
+                  git
+                  zip
+                  unzip
+                  nodejs_22
+                  bun
+                  bottom
+                  fd
+                  fzf
+                  jq
+                  ripgrep
+                  sad
+                  yazi
+                  gcc
+                  python312
+                  tmux
+                  cargo
+                  rustc
+                  rustup
+                  lazygit
+                  wget
+                  luarocks
+                  lua-language-server
+                  luajitPackages.lua-lsp
+                ];
+
+                programs.neovim = {
+                  enable = true;
+                  defaultEditor = true;
+                  viAlias = true;
+                  vimAlias = true;
+                };
+
+                services.openssh = {
+                  enable = true;
+                };
+
+                systemd.services.wsl-keep-running = {
+                  description = "Keep NixOS-WSL running";
+                  wantedBy = [ "multi-user.target" ];
+                  serviceConfig = {
+                    Type = "simple";
+                    ExecStart = "${pkgs.coreutils}/bin/sleep infinity";
+                    Restart = "always";
+                  };
+                };
+              }
+            ];
+        };
+      };
+    };
+}
