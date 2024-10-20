4fd4eefaab7008e19578bf5dc422384abbb707e8
paradox8599 <paradox8599@outlook.com>
2024-10-20 18:46:57 +1100

    zoxide

--- home/nixos/default.nix
+++ home/nixos/default.nix
@@ -2,6 +2,7 @@
 {
   home.username = "nixos";
   home.homeDirectory = "/home/nixos";
+  home.stateVersion = "24.11";
 
   # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
@@ -58,42 +59,60 @@
     alejandra
   ];
 
-  # basic configuration of git, please change to your own
-  programs.git = {
-    enable = true;
-    userName = "paradox8599";
-    userEmail = "paradox8599@outlook.com";
-  };
+  programs = {
+    home-manager.enable = true;
+    git = {
+      enable = true;
+      userName = "paradox8599";
+      userEmail = "paradox8599@outlook.com";
+    };
 
-  programs.starship = {
-    enable = true;
-    # custom settings
-    settings = {
-      add_newline = false;
-      aws.disabled = true;
-      gcloud.disabled = true;
-      line_break.disabled = true;
+    starship = {
+      enable = true;
+      # custom settings
+      settings = {
+        add_newline = false;
+        aws.disabled = true;
+        gcloud.disabled = true;
+        line_break.disabled = true;
+      };
+    };
+
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+
+      profileExtra = ''
+        function y() {
+          local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+          yazi "$@" --cwd-file="$tmp"
+          if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+            builtin cd -- "$cwd"
+          fi
+          rm -f -- "$tmp"
+        }
+      '';
+
+      shellAliases = {
+        urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
+        urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
+        nixhistory = "nix profile history --profile /nix/var/nix/profiles/system";
+        lg = "lazygit";
+        rb = "sudo nixos-rebuild --flake ~/.config/nixos";
+        rbv = "sudo nixos-rebuild --flake ~/.config/nixos --show-trace --print-build-logs --verbose";
+        t = ''[[ $(tmux ls 2>/dev/null | rg -v attached | wc -l) -gt 0 ]] && tmux attach -t $(tmux ls | rg -v attach | cut -d":" -f1 | tr "\n" " " | cut -d" " -f1) || tmux -u new-session -s main'';
+        tl = "tmux ls 2>/dev/null";
+        j = "z";
+        ji = "zi";
+      };
+    };
+
+    zoxide = {
+      enable = true;
+      enableZshIntegration = true;
+      enableNushellIntegration = true;
     };
   };
-
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-
-    shellAliases = {
-      urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
-      urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
-      nixhistory = "nix profile history --profile /nix/var/nix/profiles/system";
-      lg = "lazygit";
-      rb = "sudo nixos-rebuild --flake ~/.config/nixos";
-      rbv = "sudo nixos-rebuild --flake ~/.config/nixos --show-trace --print-build-logs --verbose";
-      t = ''[[ $(tmux ls 2>/dev/null | rg -v attached | wc -l) -gt 0 ]] && tmux attach -t $(tmux ls | rg -v attach | cut -d":" -f1 | tr "\n" " " | cut -d" " -f1) || tmux -u new-session -s main'';
-      tl = "tmux ls 2>/dev/null";
-    };
-  };
-
-  home.stateVersion = "24.11";
-  programs.home-manager.enable = true;
 }
