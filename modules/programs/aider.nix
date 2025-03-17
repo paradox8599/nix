bc41d2acda68d6b46f2807e43d50470c62c96c2a
paradox8599 <paradox8599@outlook.com>
2025-03-17 23:11:48 +1100

    aider with playwright

--- modules/programs/aider.nix
+++ modules/programs/aider.nix
@@ -2,12 +2,14 @@
 {
   home = {
     packages = with pkgs; [
-      aider-chat
+      aider-chat.withPlaywright
     ];
+
     file.".aider.conf.yml" = {
       enable = true;
       source = ../../configs/aider/.aider.conf.yml;
     };
+
     file.".aider.model.metadata.json" = {
       enable = true;
       source = ../../configs/aider/.aider.model.metadata.json;
