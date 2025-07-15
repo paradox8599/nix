12ca5f5915466272ddf38941c867ff3e161c4c01
paradox8599 <paradox8599@outlook.com>
2025-07-15 18:54:00 +1000

    refactor: remove tmate config from tmux module

--- modules/programs/tmux.nix
+++ modules/programs/tmux.nix
@@ -71,39 +71,4 @@
     '';
   };
 
-  programs.tmate = {
-    enable = true;
-
-    extraConfig = ''
-      set -g default-terminal "screen-256color"
-      set -ga terminal-overrides ",*256col*:Tc"
-      set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
-      set-environment -g COLORTERM "truecolor"
-
-      set-option -g mouse on
-
-      # yazi
-      set -g allow-passthrough on
-      set -ga update-environment TERM
-      set -ga update-environment TERM_PROGRAM
-
-      # hjkl to select and y to copy in copy mode
-      set-window-option -g mode-keys vi
-      bind-key -T copy-mode-vi v send-keys -X begin-selection
-      bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
-      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
-
-      bind 'j' swap-pane -D
-      bind 'k' swap-pane -U
-
-      # split window at current path
-      bind 'v' split-window -v -c "#{pane_current_path}"
-      bind 'h' split-window -h -c "#{pane_current_path}"
-
-      # change prefix key
-      unbind C-b
-      set -g prefix C-a
-      bind C-a send-prefix
-    '';
-  };
 }
