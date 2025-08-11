7ce92b39ae8135881083a28703dca1fd006b677b
paradox8599 <paradox8599@outlook.com>
2025-08-11 22:52:13 +1000

    chore: Update nixpkgs-aider and home-manager revisions

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-aider.url = "github:NixOS/nixpkgs/94def634a20494ee057c76998843c015909d6311";
+    nixpkgs-aider.url = "github:NixOS/nixpkgs/85dbfc7aaf52ecb755f87e577ddbe6dbbdbc1054";
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
     nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
     nix-darwin.url = "github:lnl7/nix-darwin";
